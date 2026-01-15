56a9c0ededf8460234b1c41452d7abe7421ceca1
Javier Etulain <javieretulain13@gmail.com>
2026-01-15 20:09:36 -0300

    Add metrics and modify colors

--- app.py
+++ app.py
@@ -1,7 +1,35 @@
 import streamlit as st
+import pandas as pd
+from pathlib import Path
+
+BASE_DIR = Path(__file__).parent
+
+def load_data():
+    data = {
+        "balance": 0,
+        "total_deposited": 0,
+        "total_withdrawn": 0
+    }
+
+    try:
+        with open("data.txt", "r") as file:
+            for line in file:
+                key, value = line.strip().split("=")
+                data[key] = int(value)
+    except FileNotFoundError:
+        pass
+
+    return data
+
+def save_data(balance, total_dep, total_wit):
+    with open("data.txt", "w") as file:
+        file.write(f"balance={balance}\n")
+        file.write(f"total_deposited={total_dep}\n")
+        file.write(f"total_withdrawn={total_wit}\n")
+
 
 with open("balance.txt", "r", encoding="utf-8") as f:
-    inity_balance = f.read()
+    st.session_state.balance = f.read()
 
 if "show_history" not in st.session_state:
     st.session_state.show_history = False
@@ -9,16 +37,22 @@ if "show_history" not in st.session_state:
 with open("history.txt", "r", encoding="utf-8") as f:
     history = f.read()
 
+if "total_deposited" not in st.session_state:
+    st.session_state.total_deposited = 0
+
+if "total_withdrawn" not in st.session_state:
+    st.session_state.total_withdrawn = 0
+
 st.markdown(
     f"""
-    <p style='font-size:22px; color:#44241A; font-weight:bold; text-align:center;'>
-        Balance: {inity_balance}
+    <p style='font-size:22px; color:#391560; font-weight:bold; text-align:center;'>
+        Balance: {st.session_state.balance} $
     </p>
     """,
     unsafe_allow_html=True
 )
 
-money = int(st.number_input("Enter a number", min_value=0,step=1000))
+money = st.number_input("Enter a number", min_value=0,step=1000)
 
 def save_history(text):
     with open ("history.txt", "a") as file:
@@ -37,25 +71,31 @@ def see_history():
         st.info("History. There is not registred operations")
 
 def deposit():
-    global inity_balance
     global money
     
     if (money == 0):
         st.warning("Enter a valid number") 
     else:
-        inity_balance = int(inity_balance) + money
-        save_history(f"DEPOSIT: +{money} | ACTUAL BALANCE: {inity_balance}")
-
+        st.session_state.total_deposited += money
+        st.session_state.balance = int(st.session_state.balance) + money
+        save_data(st.session_state.balance, 
+                  st.session_state.total_deposited, 
+                  st.session_state.total_withdrawn)
+        save_history(f"DEPOSIT: +{money} | ACTUAL BALANCE: {st.session_state.balance}")
+        
 def withdraw():
-    global inity_balance
     global money
-    if (money > int(inity_balance)):
+    if (money > int(st.session_state.balance)):
         st.warning("Not enough balance")
     elif (money == 0):
         st.warning("Enter a valid number")    
     else:
-        inity_balance = int(inity_balance) - money
-        save_history(f"WITHDRAW: -{money} | ACTUAL BALANCE: {inity_balance}")
+        st.session_state.balance = int(st.session_state.balance) - money
+        st.session_state.total_withdrawn += money
+        save_data(st.session_state.balance, 
+                  st.session_state.total_deposited, 
+                  st.session_state.total_withdrawn)
+        save_history(f"WITHDRAW: -{money} | ACTUAL BALANCE: {st.session_state.balance}")
 
 def save_balance(balance):
     with open("balance.txt", "w") as file:
@@ -64,18 +104,27 @@ def save_balance(balance):
 def clean_history():
         open("history.txt", "w").close()
         st.info("History successfully deleted")
-   
+ 
+if "initialized" not in st.session_state:
+    data = load_data()
+
+    st.session_state.balance = data["balance"]
+    st.session_state.total_deposited = data["total_deposited"]
+    st.session_state.total_withdrawn = data["total_withdrawn"]
+
+    st.session_state.initialized = True
+  
 col1, col2, col3, col4 = st.columns(4)
 
 with col1: 
     if st.button("Deposit"):
         deposit()
-        save_balance(inity_balance)
+        save_balance(st.session_state.balance)
 
 with col2:
     if st.button("Withdraw"):
         withdraw()
-        save_balance(inity_balance) 
+        save_balance(st.session_state.balance) 
 
 with col3:
     if st.button("History"):
@@ -88,3 +137,26 @@ with col4:
     if st.button("Clean history"):
         clean_history()
 
+col1_1, col2_1, col3_1 = st.columns(3)
+
+with col1_1:
+    st.metric("Current balance", f"${st.session_state.balance}")
+    
+with col2_1:
+    st.metric("Total deposited", f"${st.session_state.total_deposited}") 
+    
+with col3_1:
+    st.metric("Total withdrawn", f"${st.session_state.total_withdrawn}")
+
+data = {
+    "Metric": ["Balance", "Total Deposited", "Total Withdrawn"],
+    "Amount": [
+        st.session_state.balance,
+        st.session_state.total_deposited,
+        st.session_state.total_withdrawn
+    ]
+}
+
+df = pd.DataFrame(data).set_index("Metric")
+
+st.bar_chart(df)
--- pages/trasfers.py
+++ pages/trasfers.py
@@ -1,3 +1,6 @@
 import streamlit as st
+from pathlib import Path
+
+BASE_DIR = Path(__file__).parent
 
 st.write("This is the Transfers page.")
