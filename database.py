ec81b4899406fc15ef345db47f6e116e1988f065
Javier Etulain <javieretulain13@gmail.com>
2026-01-18 18:10:49 -0300

    Triying to fix errors

--- database.py
+++ database.py
@@ -2,17 +2,11 @@ import psycopg2
 import streamlit as st
 
 def get_connection():
-    try:
-        conn = psycopg2.connect(
-            host=st.secrets["db_host"],
-            database=st.secrets["db_name"],
-            user=st.secrets["db_user"],
-            password=st.secrets["db_password"],
-            port=st.secrets["db_port"],
-            sslmode="require"
-        )
-        return conn
-    except Exception as e:
-        import streamlit as st
-        st.error(str(e))
-        raise
+    return psycopg2.connect(
+        host=st.secrets["db_host"],
+        database=st.secrets["db_name"],
+        user=st.secrets["db_user"],
+        password=st.secrets["db_password"],
+        port=st.secrets["db_port"],
+        sslmode="require"
+    )
